71fe806b0b53595799e66f999b5638cf7406e24d
Mike Robinson <multiwebinc@users.noreply.github.com>
2021-12-08 11:19:29 -0600

    Update create_attributes_table.php

--- updates/v1.0.1/create_attributes_table.php
+++ updates/v1.0.1/create_attributes_table.php
@@ -8,15 +8,17 @@ class CreateAttributesTable extends Migration
 
     public function up()
     {
-        Schema::create('rainlab_translate_attributes', function($table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->string('locale')->index();
-            $table->string('model_id')->index()->nullable();
-            $table->string('model_type')->index()->nullable();
-            $table->mediumText('attribute_data')->nullable();
-        });
+        if (!Schema::hasTable('rainlab_translate_attributes')) {
+            Schema::create('rainlab_translate_attributes', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->string('locale')->index();
+                $table->string('model_id')->index()->nullable();
+                $table->string('model_type')->index()->nullable();
+                $table->mediumText('attribute_data')->nullable();
+            });
+        }
     }
 
     public function down()
